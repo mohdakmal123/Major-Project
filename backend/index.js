1ce22335937781385aa286b481fcaef23175d6e3
mohdakmal123 <akmal.ahmad1209@gmail.com>
2024-10-01 18:08:06 +0530

    ..

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+// importing express
+const express = require('express');
+const UserRouter = require('./routers/userRouter');
+const BusinessRouter = require('./routers/businessRouter');
+const cors = require('cors');
+
+// initializing express
+const app = express();
+const port = 5000;
+
+// middleware
+app.use(cors({
+    origin: 'http://localhost:3000'
+})
+
+);
+app.use(express.json());
+app.use('/user', UserRouter);
+app.use('/business', BusinessRouter);
+
+// route or endpoint
+app.get('/', (req, res) => {
+    res.send('Response from express')
+})
+
+app.get('/add', (req, res) => {
+    res.send('Response from add');
+})
+
+// getall
+app.get('/getall', (req, res) => {
+    res.send('Response from getall');
+})
+
+//update
+app.get('/getall', (req, res) => {
+    res.send('Response from update');
+})
+app.listen(port, () => { console.log('server started') });
+
+
+
+
+
+
+
---
+++ backend/routers/userRouter.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const Model = require('../models/userModel');
+
+const jwt = require('jsonwebtoken');
+const verifyToken = require('./verifyToken');
+require('dotenv').config();
+
+const router = express.Router();
+
+router.post('/add', (req, res) => {
+    console.log(req.body);
+
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err.code === 11000) {
+                res.status(200).json({ message: 'Email already exists' });
+            } else {
+                res.status(500).json(err); ({ message: 'something went wrong' });
+            }
+        });
+
+});
+
+router.get('/getall', verifyToken, (req, res) => {
+
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+router.get('/getbyemail/:email', (req, res) => {
+    Model.findOne({ email: req.params.email })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/getbycity/:city', (req, res) => {
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// : denotes url parameter
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+});
+router.post('/authenticate', (req, res) => {
+    Model.findOne(req.body)
+        .then((result) => {
+            if (result) {
+
+                const { _id, name, email, password } = result;
+
+                const payload = { _id, name, email, password };
+
+
+
+                // generate token
+                jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    { expiresIn: 10 },
+                    (err, token) => {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).json(err);
+                        } else {
+
+                            res.status(200).json({ token });
+                        }
+                    }
+                )
+
+
+            } else {
+                res.status(401).json({ message: 'Invalid credentials' });
+            }
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
+})
+
+module.exports = router;
