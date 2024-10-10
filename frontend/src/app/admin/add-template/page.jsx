79275db80ddbf3f5f9eaa7113a8f5fa79585e0a6
mohdshahan <mohdshahan171@gmail.com>
2024-10-10 19:12:20 +0530

    system

--- frontend/src/app/admin/add-template/page.jsx
+++ frontend/src/app/admin/add-template/page.jsx
@@ -1,53 +1,68 @@
 'use client'
 import React, { useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 import { useFormik } from 'formik';
+
+
 const AddTemplate = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    name: '',
-    email: '',
-    image: '',
-    codeSnippet: ''
-  });
+
+  const templateForm = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      name: '',
+      author: '',
+      image: '',
+      codeSnippet: ''
+    },
+    onSubmit: (values) => {
+      console.log(values);
+
+      axios.post('http://localhost:5000/template/add', values)
+        .then((result) => {
+          console.log(result.status);
+
+          toast.success('Template added Successfully')
+        }).catch((err) => {
+          toast.error('Some Error Occured')
+        });
+    },
+  })
   const [imagePreview, setImagePreview] = useState(null);
   const [message, setMessage] = useState('');
   const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const handleChange = (e) => {
-    const { name, value, type, files } = e.target;
-    if (type === 'file') {
-      const file = files[0];
-      if (file && file.size > 2 * 1024 * 1024) {
-        alert('File size must be less than 2MB!');
-        e.target.value = ''; // Reset the file input
-        return;
-      }
-      setFormData({ ...formData, [name]: file });
-      setImagePreview(URL.createObjectURL(file)); // Set image preview
-    } else {
-      setFormData({ ...formData, [name]: value });
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    setMessage('Template added successfully!');
-    setFormData({
-      title: '',
-      description: '',
-      name: '',
-      email: '',
-      image: null,
-      codeSnippet: ''
-    });
-    setImagePreview(null);
-  };
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
+
+
+  //upload (shahan@mail.com/1234)
+  const handleUpload = (e) => {
+    const file = e.target.files[0];
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'mypreset');
+    formData.append('cloud_name', 'dalzae2bg');
+    axios.post('https://api.cloudinary.com/v1_1/dalzae2bg/image/upload', formData, {
+      headers: { 'Content-Type': 'multipart/form-data' }
+    })
+      .then((result) => {
+        console.log(result.data);
+        toast.success('File Uploaded Successfully');
+        templateForm.setFieldValue('image', result.data.url);
+
+      }).catch((err) => {
+        console.log(err);
+        toast.error('Failed To Upload File');
+
+      });
+  }
+
+
 
   return (
     <div className={`min-h-screen p-6 flex items-center justify-center ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-gray-800'}`}>
@@ -61,7 +76,7 @@ const AddTemplate = () => {
       </div>
 
       <form
-        onSubmit={handleSubmit}
+        onSubmit={templateForm.handleSubmit}
         className={`w-full max-w-xl p-6 shadow-lg rounded-lg transition-all duration-300 ease-in-out ${isDarkMode ? 'bg-black' : 'bg-white'}`}
       >
         <h1 className={`text-center text-3xl font-bold mb-8 ${isDarkMode ? 'text-white' : 'text-gray-900'}`}>
@@ -80,8 +95,8 @@ const AddTemplate = () => {
               placeholder="Enter Title"
               id="title"
               className={`w-full mt-2 p-2 border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
-              value={formData.title}
-              onChange={handleChange}
+              value={templateForm.values.title}
+              onChange={templateForm.handleChange}
               required
             />
           </div>
@@ -97,11 +112,32 @@ const AddTemplate = () => {
               rows={5}
               placeholder="Write your description..."
               className={`w-full mt-2 p-2 border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
-              value={formData.description}
-              onChange={handleChange}
+              value={templateForm.values.description}
+              onChange={templateForm.handleChange}
               required
             />
           </div>
+
+
+
+          <div>
+            <label htmlFor="author" className={`block text-lg font-semibold ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
+              Author:
+            </label>
+            <textarea
+              id="author"
+              name="author"
+              cols={30}
+              rows={5}
+              placeholder="author..."
+              className={`w-full mt-2 p-2 border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
+              value={templateForm.values.author}
+              onChange={templateForm.handleChange}
+              required
+            />
+          </div>
+
+
 
           <div>
             <label htmlFor="name" className={`block text-lg font-semibold ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
@@ -113,27 +149,13 @@ const AddTemplate = () => {
               placeholder="Enter Your Name"
               id="name"
               className={`w-full mt-2 p-2 border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
-              value={formData.name}
-              onChange={handleChange}
+              value={templateForm.values.name}
+              onChange={templateForm.handleChange}
               required
             />
           </div>
 
-          <div>
-            <label htmlFor="email" className={`block text-lg font-semibold ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
-              Email:
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Enter Your Email"
-              id="email"
-              className={`w-full mt-2 p-2 border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
+
 
           <div>
             <label htmlFor="image" className={`block text-lg font-semibold ${isDarkMode ? 'text-white' : 'text-gray-800'}`}>
@@ -144,13 +166,11 @@ const AddTemplate = () => {
               name="image"
               id="image"
               className={`mt-2 w-full border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
-              onChange={handleChange}
+              onChange={handleUpload}
               accept="image/*"
               required
             />
-            {imagePreview && (
-              <img src={imagePreview} alt="Preview" className="mt-4 rounded-md h-32 object-cover" />
-            )}
+
           </div>
 
           <div>
@@ -164,13 +184,13 @@ const AddTemplate = () => {
               rows={5}
               placeholder="Write your code here..."
               className={`w-full mt-2 p-2 border rounded-md focus:outline-none ${isDarkMode ? 'bg-gray-700 border-gray-600 text-white' : 'bg-gray-200 border-gray-300 text-gray-800'} focus:ring-2 focus:ring-indigo-500`}
-              value={formData.codeSnippet}
-              onChange={handleChange}
+              onChange={templateForm.handleChange}
+              value={templateForm.values.codeSnippet}
               required
             />
           </div>
 
-          <button className="w-full py-3 px-6 rounded-md text-lg font-semibold text-white bg-indigo-600 hover:bg-indigo-700 transition-all duration-300">
+          <button type="submit" className="w-full py-3 px-6 rounded-md text-lg font-semibold text-white bg-indigo-600 hover:bg-indigo-700 transition-all duration-300">
             Add Template
           </button>
         </div>
